103dffe7523861705c29aea253230cddea9d6618
Ali Farrokhtala <farrokhtala@gmail.com>
2024-12-19 23:33:32 -0800

    added test script

--- backend/core/views.py
+++ backend/core/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
 
 from core.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
 from core.models import Product, Order, OrderItem
@@ -27,6 +29,7 @@ class OrderListAPIView(generics.ListAPIView):
 class UserOrderListAPIView(generics.ListAPIView):
     queryset = Order.objects.prefetch_related("items", "items__product").all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         qs = super().get_queryset()
