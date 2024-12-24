144f48156d7688e7673629e46c695242bf809666
Ali Farrokhtala <farrokhtala@gmail.com>
2024-12-24 00:14:39 -0800

    fixed an error in the test file

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -21,12 +21,12 @@ class UserOrderTestCase(TestCase):
     def test_user_order_endpoint_retrieves_only_authenticated_user_orders(self):
         user = User.objects.get(username="user2")
         self.client.force_login(user)
-        response = self.client.get(reverse("user-orders"))
+        response = self.client.get(reverse("orders"))
 
         assert response.status_code == status.HTTP_200_OK
         orders = response.json()
         self.assertTrue(all(order["user"] == user.id for order in orders))
 
     def test_user_order_list_unauthenticated(self):
-        response = self.client.get(reverse("user-orders"))
+        response = self.client.get(reverse("orders"))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
