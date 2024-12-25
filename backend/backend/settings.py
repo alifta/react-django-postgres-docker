42aa06b29d7f91515731feaefb7a9726677e7dc0
Ali Farrokhtala <farrokhtala@gmail.com>
2024-12-25 14:27:11 -0800

    changed lint to ruff from flake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,10 +92,10 @@ DATABASES = {
     # }
     "default": {
         "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("DB_HOST"),
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASS"),
-        "HOST": os.environ.get("DB_HOST"),
         "PORT": "5432",
     }
 }
