90a6df2c7e589c79a1cea3531ac6d1d08225e3fd
Ali Farrokhtala <farrokhtala@gmail.com>
2024-12-25 14:23:37 -0800

    changed db version in docker compose file

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
 
 from core.models import Order, User
 
