ec0b219ac52c94d30697f2dd2106230cf93daa5c
Ali Farrokhtala <farrokhtala@gmail.com>
2025-01-16 21:51:14 -0800

    removed frontend service from docker compose to avoid develop not allowed error in production

---
+++ backend/core/tests/test_health_check.py
@@ -0,0 +1,23 @@
+"""
+Tests for the health check API.
+"""
+
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class HealthCheckTestCase(TestCase):
+    """Test the health check API."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_health_check(self):
+        """Test the health check API."""
+        url = reverse("health-check")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {"status": "ok"})
