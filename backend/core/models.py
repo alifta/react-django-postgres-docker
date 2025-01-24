e6b8e0b1799cce8c0d6dcbe4383693e6814450e0
Ali Farrokhtala <ali@Alis-MacBook-Air.local>
2025-01-24 14:34:32 -0800

    added some extra fileds to the user_profiles table

--- backend/core/models.py
+++ backend/core/models.py
@@ -13,6 +13,10 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 
+# Models tha we can add later:
+# - Media (Images, Videos, etc.) for users, peroperties or projects
+#     - updated_by, created_by, created_at, updated_at, media_category, media_group, media_type, media_name, media_description, media_url, media_status, media_tags
+
 
 def recipe_image_file_path(instance, filename):
     """Generate file path for new recipe image."""
@@ -25,26 +29,27 @@ def recipe_image_file_path(instance, filename):
 class UserManager(BaseUserManager):
     """Manager for users."""
 
-    def create_user(self, email, password=None, **extra_fields):
-        """Create, save and return a new user."""
+    def create_user(self, username, email, password=None, **extra_fields):
+        """Create and return a new user."""
         if not email:
-            raise ValueError("User must have an email address.")
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+            raise ValueError("The Email field must be set.")
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            **extra_fields,
+        )
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, username, email, password=None, **extra_fields):
         """Create and return a new superuser."""
-        # extra_fields.setdefault("is_staff", True)
-        # extra_fields.setdefault("is_superuser", True)
-        user = self.create_user(email, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-        return user
+        return self.create_user(username, email, password, **extra_fields)
 
 
 # USER
@@ -52,21 +57,29 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     """User model."""
 
-    # user_id = models.BigAutoField(primary_key=True)
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(
-        max_length=255, blank=True
-    )  # TODO: Deprecate this field and move it to UserProfile or change it to user_name
+    STATUS_CHOICES = (
+        ("active", "Active"),
+        ("inactive", "Inactive"),
+        ("pending", "Pending"),
+        ("suspended", "Suspended"),
+        ("deleted", "Deleted"),
+    )
+
+    user_id = models.BigAutoField(primary_key=True)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="active")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     # password_hash = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # cv = models.FileField(upload_to="cvs/", blank=True, null=True)
-
-    USERNAME_FIELD = "email"
 
     objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
 
     class Meta:
         db_table = "users"
@@ -81,12 +94,22 @@ class UserProfile(models.Model):
 
     profile_id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, db_column="user_id")
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    # office = models.ForeignKey(Office, on_delete=models.CASCADE, db_column="office_id")
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
     phone_number = models.CharField(max_length=50, blank=True, null=True)
+    mobile_phone = models.CharField(max_length=50, blank=True, null=True)
     profile_picture = models.CharField(max_length=255, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
+    cv = models.CharField(max_length=255, blank=True, null=True)
+    job_title = models.CharField(max_length=100, blank=True, null=True)
+    address1 = models.CharField(max_length=255, blank=True, null=True)
+    address2 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -547,7 +570,7 @@ class Order(models.Model):
     )
 
     def __str__(self):
-        return f"Order {self.order_id } by {self.user.email}"
+        return f"Order {self.order_id} by {self.user.email}"
 
 
 class OrderItem(models.Model):
